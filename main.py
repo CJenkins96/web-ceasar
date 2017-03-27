d612aaa0940da6256d3e8f30d296b06bc819371a
IceBladeNetwork <TheIceBladeNetwork@gmail.com>
2017-03-27 17:45:06 -0500

    Added validation for rotation

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ form = """
     <body>
     <form method="post">
         <label>Rotate by:</label>
-        <input type="number" name="rotation">
+        <input type="number" name="rotation"><span style="color:red;">%(error)s</span>
         <br>
         <label>Type a message:</label>
-        <textarea name="message">%s</textarea>
+        <textarea name="message">%(encrypted)s</textarea>
 
         <br>
         <input type="submit" value="ENCRYPT!">
@@ -22,17 +22,20 @@ form = """
 </html>
 """
 class MainHandler(webapp2.RequestHandler):
-    def write_form(self, encrypted = ""):    
-        self.response.write(form % encrypted)
+    def write_form(self, encrypted = "", error = ""):    
+        self.response.write(form % {"error": error, "encrypted": encrypted})
 
     def get(self):
         self.write_form()
 
     def post(self):
         message = self.request.get("message")
-        rotate = int(self.request.get("rotation"))
-        encrypted_message = ceasar.encrypt(message, rotate)
-        self.write_form(cgi.escape(encrypted_message))
+        if self.request.get("rotation").isdigit():
+            rotate = int(self.request.get("rotation"))
+            encrypted_message = ceasar.encrypt(message, rotate)
+            self.write_form(cgi.escape(encrypted_message))
+        else:
+            self.write_form(message, "  That's not a valid number!")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
