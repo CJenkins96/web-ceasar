a7e94658f5aaa3c4f0fc7533e25395b3d798fbb1
TheIceBladeNetwork <TheIceBladeNetwork@gmail.com>
2017-03-27 14:55:58 -0500

    Added the form

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import webapp2
 import ceasar
+import cgi
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "Hello, world!"
+        message = ""
         encrypted_message = ceasar.encrypt(message, 13)
-        self.response.write(encrypted_message)
+        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        button = "<input type='submit' value='ENCRYPT!'>"
+        form = "<form>" + textarea + button + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
